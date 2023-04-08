1514583676a79da4093bfa51d67d423301a5aec1
Will Hopkins <willothyh@gmail.com>
2023-04-08 16:05:24 -0700

    feat: command passthrough
    * Use +<cmd> to execute a command in the host after opening files
        * useful for manpages - `export MANPAGER="nvim +Man!"`
    * Use --cmd <cmd> to execute a command in the host before opening files
    
    fixes #32, fixes #36

--- lua/flatten/guest.lua
+++ lua/flatten/guest.lua
@@ -1,41 +1,46 @@
 local M = {}
 
 local function is_windows()
-	return package.config:sub(1, 1) == '\\'
+	return string.sub(package["config"], 1, 1) == "\\"
 end
 
 local function sanitize(path)
-	return path:gsub('\\', '/')
+	return path:gsub("\\", "/")
 end
 
 local function send_files(host, files, stdin)
-	if #files < 1 and #stdin < 1 then return end
+	if #files < 1 and #stdin < 1 then
+		return
+	end
 
 	local config = require("flatten").config
 	local force_block = vim.g.flatten_wait ~= nil or config.callbacks.should_block(vim.v.argv)
 
 	local server = vim.fn.fnameescape(vim.v.servername)
-	local cwd = vim.fn.fnameescape(vim.fn.getcwd( -1, -1))
+	local cwd = vim.fn.fnameescape(vim.fn.getcwd(-1, -1))
 	if is_windows() then
 		server = sanitize(server)
 		cwd = sanitize(cwd)
 	end
 
-	local call = string.format([[return require('flatten.core').edit_files(%s)]], vim.inspect({
-		files = files,
-		response_pipe = server,
-		guest_cwd = cwd,
-		stdin = stdin,
-		argv = vim.v.argv,
-		force_block = force_block,
-	}))
+	local call = string.format(
+		[[return require('flatten.core').edit_files(%s)]],
+		vim.inspect({
+			files = files,
+			response_pipe = server,
+			guest_cwd = cwd,
+			stdin = stdin,
+			argv = vim.v.argv,
+			force_block = force_block,
+		})
+	)
 
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 		vim.api.nvim_buf_delete(buf, { force = true })
 	end
 	local block = vim.fn.rpcrequest(host, "nvim_exec_lua", call, {}) or force_block
 	if not block then
-		vim.cmd('qa!')
+		vim.cmd("qa!")
 	end
 	vim.fn.chanclose(host)
 	while true do
@@ -47,31 +52,33 @@ M.init = function(host_pipe)
 	-- Connect to host process
 	local host = vim.fn.sockconnect("pipe", host_pipe, { rpc = true })
 	-- Exit on connection error
-	if host == 0 then vim.cmd("qa!") end
+	if host == 0 then
+		vim.cmd("qa!")
+	end
 
 	-- Get new files
 	local files = vim.fn.argv()
 	local nfiles = #files
 
 	vim.api.nvim_create_autocmd("StdinReadPost", {
-		pattern = '*',
+		pattern = "*",
 		callback = function()
 			local readlines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
 			send_files(host, files, readlines)
-		end
+		end,
 	})
 
 	-- No arguments, user is probably opening a nested session intentionally
 	-- Or only piping input from stdin
 	vim.api.nvim_create_autocmd("BufEnter", {
-		pattern = '*',
+		pattern = "*",
 		callback = function()
 			if nfiles < 1 then
-				vim.cmd('qa!')
+				vim.cmd("qa!")
 			end
 
 			send_files(host, files, {})
-		end
+		end,
 	})
 end
 
